d6014b8ce3cefc35508b9de0a1f4107f38e0b993
ankittripathi97 <110759846+ankittripathi97@users.noreply.github.com>
2022-08-07 22:33:47 +0530

    solving issue #3

--- src/components/Label.js
+++ src/components/Label.js
@@ -3,6 +3,6 @@ import {Text} from 'react-native';
 
 export const Label = () => {
   return   <View>
-    <Text>Checking Text</Text>
+    <Text>Solving issue 3</Text>
     <Text> Your API Call are shown below</Text></View>
 };
