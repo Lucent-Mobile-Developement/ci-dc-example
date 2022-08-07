e35386221c2822ec2f54a76d0e1bf04f515e9614
ankittripathi97 <110759846+ankittripathi97@users.noreply.github.com>
2022-08-07 22:27:58 +0530

    issue resolve #1

--- src/components/Label.js
+++ src/components/Label.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Text} from 'react-native';
 
 export const Label = () => {
-  return <Text> Your photos will shown below</Text>;
-  
+  return   <View>
+    <Text>Checking Text</Text>
+    <Text> Your API Call are shown below</Text></View>
 };
