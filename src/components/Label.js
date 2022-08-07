71ccf7cba2f10d4e014972be5514de57addcda8c
ankittripathi97 <110759846+ankittripathi97@users.noreply.github.com>
2022-08-07 19:43:38 +0530

    Update Label.js

--- src/components/Label.js
+++ src/components/Label.js
@@ -2,5 +2,6 @@ import React from 'react';
 import {Text} from 'react-native';
 
 export const Label = () => {
-  return <Text> Your API Call are shown below</Text>;
+  return <Text> Your photos will shown below</Text>;
+  
 };
